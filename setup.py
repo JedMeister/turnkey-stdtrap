5b963b073822f68d8a2b71d2be196803d6f6e464
Stefan Davis <stefan@turnkeylinux.org>
2019-06-26 03:18:28 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-stdtrap',
+    version='1.0',
+    description='A std[out/err] trapping library',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-stdtrap',
+    packages=['stdtrap'])
